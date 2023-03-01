3b27144396a0651c849be3047c64dcca822bc2cd
ngardunia <nickrhyse@gmail.com>
2023-03-01 15:03:16 -0500

    added putDB code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+const jateDB = await openDB('jate', 1);
+const tx = jateDB.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.add({ jate: content });
+const result = await request;
+console.log('🚀 - data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
